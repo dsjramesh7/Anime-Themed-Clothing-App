307ba85dbce33f6bf3e4c5b6cccda8576d2b38db
dsjramesh7 <rameshdussa07@gmail.com>
2024-12-19 14:36:49 +0530

    adding routing and components and pages created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Checkout from "./pages/Checkout";
+import Cart from "./pages/Cart";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div>
-      <h1 className="bg-red-500">Hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product" element={<Cart />} />
+        <Route path="/product" element={<Checkout />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Navbar;
